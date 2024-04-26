4e1134c5af0d59baf1035871eaea9b88d1e39a77
albatellouc3m <100495755@alumnos.uc3m.es>
2024-04-26 21:44:13 +0200

    ALL

--- src/main/python/uc3m_travel/hotel_manager.py
+++ src/main/python/uc3m_travel/hotel_manager.py
@@ -1,7 +1,6 @@
 
 """Module for the hotel manager"""
 import json
-from uc3m_travel.hotel_management_exception import HotelManagementException
 from uc3m_logistics.attribute_arrival_date import ArrivalDate
 from uc3m_logistics.attribute_credit_card import CreditCard
 from uc3m_logistics.attribute_idcard import IDCard
@@ -14,105 +13,89 @@ from uc3m_travel.storage.reservation_json_store import ReservationJsonStore
 from uc3m_travel.storage.stay_json_store import StayJsonStore
 from uc3m_travel.hotel_reservation import HotelReservation
 from uc3m_travel.hotel_stay import HotelStay
-
+from uc3m_travel.hotel_management_exception import HotelManagementException
 class HotelManager:
-    class __HotelManager:
-        """Class with all the methods for managing reservations and stays"""
-        _instance = None
+    """Class with all the methods for managing reservations and stays"""
+    _instance = None
+    def __init__(self):
+        if not hasattr(self, 'initialized'):
+            self.initialized = True
 
-        def __new__(cls, *args, **kwargs):
-            if not cls._instance:
-                cls._instance = super().__new__(cls, *args, **kwargs)
-            return cls._instance
+    def load_json_store(self, file_path):
+        try:
+            with open(file_path, "r", encoding="utf-8", newline="") as file:
+                file_contents = json.load(file)
+        except FileNotFoundError:
+            raise HotelManagementException("Wrong file or file path")
+        except json.JSONDecodeError as ex:
+            raise HotelManagementException("JSON Decode Error - Wrong JSON Format") from ex
+        return file_contents
 
-        def __init__(self):
-            if not hasattr(self, 'initialized'):
-                self.initialized = True
+    # pylint: disable=too-many-arguments
+    def room_reservation(self,
+                         credit_card: str,
+                         name_surname: str,
+                         id_card: str,
+                         phone_number: str,
+                         room_type: str,
+                         arrival_date: str,
+                         num_days: int) -> str:
 
-        def load_json_store(self, file_path):
-            try:
-                with open(file_path, "r", encoding="utf-8", newline="") as file:
-                    file_contents = json.load(file)
-            except FileNotFoundError:
-                raise HotelManagementException("Wrong file or file path")
-            except json.JSONDecodeError as ex:
-                raise HotelManagementException("JSON Decode Error - Wrong JSON Format") from ex
-            return file_contents
+        id_card = IDCard(id_card)
+        room_type = RoomType(room_type)
+        name_surname = NameSurname(name_surname)
+        credit_card = CreditCard(credit_card)
+        arrival_date = ArrivalDate(arrival_date)
+        num_days = NumDays(num_days)
+        phone_number = PhoneNumber(phone_number)
+        my_reservation = HotelReservation(id_card=id_card.attr_value,
+                                          credit_card_number=credit_card.attr_value,
+                                          name_surname=name_surname.attr_value,
+                                          phone_number=phone_number.attr_value,
+                                          room_type=room_type.attr_value,
+                                          arrival=arrival_date.attr_value,
+                                          num_days=num_days.attr_value)
 
-        # pylint: disable=too-many-arguments
-        def room_reservation(self,
-                             credit_card: str,
-                             name_surname: str,
-                             id_card: str,
-                             phone_number: str,
-                             room_type: str,
-                             arrival_date: str,
-                             num_days: int) -> str:
+        # escribo el fichero Json con todos los datos
+        existing_reservations = ReservationJsonStore()
 
-            id_card = IDCard(id_card)
-            room_type = RoomType(room_type)
-            name_surname = NameSurname(name_surname)
-            credit_card = CreditCard(credit_card)
-            arrival_date = ArrivalDate(arrival_date)
-            num_days = NumDays(num_days)
-            phone_number = PhoneNumber(phone_number)
-            my_reservation = HotelReservation(id_card=id_card.attr_value,
-                                              credit_card_number=credit_card.attr_value,
-                                              name_surname=name_surname.attr_value,
-                                              phone_number=phone_number.attr_value,
-                                              room_type=room_type.attr_value,
-                                              arrival=arrival_date.attr_value,
-                                              num_days=num_days.attr_value)
+        # compruebo que esta reserva no esta en la lista
+        if existing_reservations.find_reservation("_HotelReservation__localizer",my_reservation.localizer):
+            raise HotelManagementException("Reservation already exists")
+        if existing_reservations.find_reservation("_HotelReservation__id_card", my_reservation.id_card):
+            raise HotelManagementException("This ID card has another reservation")
+        # añado los datos de mi reserva a la lista , a lo que hubiera
+        existing_reservations.add(my_reservation.__dict__)
+        return my_reservation.localizer
 
-            # escribo el fichero Json con todos los datos
-            existing_reservations = ReservationJsonStore()
+    def guest_arrival(self, file_input: str) -> str:
+        """Manages the arrival of a guest with a reservation."""
+        try:
+            input_list = self.load_json_store(file_input)
+            my_localizer = input_list["Localizer"]
+            my_id_card = input_list["IdCard"]
+        except KeyError as e:
+            raise HotelManagementException(
+                "Error - Invalid Key in JSON")
 
-            # compruebo que esta reserva no esta en la lista
-            if existing_reservations.find_reservation("_HotelReservation__localizer",my_reservation.localizer):
-                raise HotelManagementException("Reservation already exists")
-            if existing_reservations.find_reservation("_HotelReservation__id_card", my_reservation.id_card):
-                raise HotelManagementException("This ID card has another reservation")
-            # añado los datos de mi reserva a la lista , a lo que hubiera
-            existing_reservations.add(my_reservation.__dict__)
-            return my_reservation.localizer
+        my_id_card = IDCard(my_id_card).attr_value
+        my_localizer = Localizer(my_localizer).attr_value
 
-        def guest_arrival(self, file_input: str) -> str:
-            """Manages the arrival of a guest with a reservation."""
-            try:
-                input_list = self.load_json_store(file_input)
-                my_localizer = input_list["Localizer"]
-                my_id_card = input_list["IdCard"]
-            except KeyError as e:
-                raise HotelManagementException(
-                    "Error - Invalid Key in JSON")
+        # Generar la room key y verificar el check-in
+        new_checkin_record = HotelStay(my_id_card, my_localizer)
+        stay_json_store = StayJsonStore()
 
-            my_id_card = IDCard(my_id_card).attr_value
-            my_localizer = Localizer(my_localizer).attr_value
+        if stay_json_store.find_stay("room_key", new_checkin_record.room_key):
+            raise HotelManagementException("checkin already exists")
 
-            # Generar la room key y verificar el check-in
-            new_checkin_record = HotelStay(my_id_card, my_localizer)
-            stay_json_store = StayJsonStore()
+        stay_json_store.add(new_checkin_record.__dict__)
+        return new_checkin_record.room_key
 
-            if stay_json_store.find_stay("room_key", new_checkin_record.room_key):
-                raise HotelManagementException("checkin already exists")
+    def guest_checkout(self, room_key: str) -> bool:
+        """manages the checkout of a guest"""
+        HotelStay.get_stay(room_key)
+        HotelStay.checkout(room_key)
+        return True
 
-            stay_json_store.add(new_checkin_record.__dict__)
-            return new_checkin_record.room_key
+    instance = None
 
-        def guest_checkout(self, room_key: str) -> bool:
-            """manages the checkout of a guest"""
-            HotelStay.get_stay(room_key)
-            HotelStay.checkout(room_key)
-            return True
-
-        instance = None
-        def __new__(cls):
-            if not HotelManager.instance:
-                HotelManager.instance = HotelManager.__HotelManager()
-            return HotelManager.instance
-
-        def __getattr__(self, name):
-            return getattr(self.instance, name)
-
-        def __setattr__(self, name, value):
-            return setattr(self.instance, name, value)
